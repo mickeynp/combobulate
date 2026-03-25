5ad763a17e4dee01c8529cd69161944966a5ca6b
xvw <xavier.vdw@gmail.com>
2026-03-25 07:51:18 +0100

    Add OCaml grammars (and related)
    Co-authored-by: Tim McGilchrist <timmcgil@gmail.com>
    Co-authored-by: PizieDust <playersrebirth@gmail.com>

--- .ts-test.el
+++ .ts-test.el
@@ -2,3 +2,14 @@
 (setq load-prefer-newer t)
 (princ (format "Default directory is `%s'\n" default-directory))
 (load-library "tests/html-ts-mode/html-ts-mode.el")
+(load-library "tests/tuareg/tuareg-opam.el")
+(load-library "tests/tuareg/tuareg-compat.el")
+(load-library "tests/tuareg/tuareg.el")
+;; (load-library "tests/ocaml-ts-mode/ocaml-ts-mode.el")
+(load-library "tests/tuareg-treesit.el")
+
+
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?\\'" . tuareg-mode)
+                ("\\.topml\\'" . tuareg-mode))
+              auto-mode-alist))
