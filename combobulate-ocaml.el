a913d9358989a2530f472dfe2a5d3ea8b52786cb
PizieDust <playersrebirth@gmail.com>
2026-05-15 03:18:01 +0100

    add loop/control nodes to procedures defun

--- combobulate-ocaml.el
+++ combobulate-ocaml.el
@@ -207,7 +207,8 @@
           ((:nodes ("type_definition" "exception_definition" "external"
                     "value_definition" "method_definition"
                     "instance_variable_definition" "module_definition"
-                    "module_type_definition" "class_definition"))))))
+                    "module_type_definition" "class_definition" "while_expression" "for_expression" 
+                    "match_expression" "if_expression"))))))
 
       (procedures-sibling
        '(
